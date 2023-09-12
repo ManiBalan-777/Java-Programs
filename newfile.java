fb5b69bbb2619bf2ead4197e27d76b8dc7fe8f0f
ManiBalan-777 <jayabalan2475@gmail.com>
2023-09-12 09:25:22 -0700

    newfile program   addded

---
+++ newfile.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+public class newfile {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("what is your name!");
+		String name =scanner.nextLine();
+		System.out.println(name);
+	}
+}

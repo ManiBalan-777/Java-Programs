da8e3dae7f2e1b17f4ab8d2dc143439d51bd5433
ManiBalan-777 <jayabalan2475@gmail.com>
2023-09-14 10:25:07 -0700

    add

---
+++ sub.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class sub
+{
+	public static void main(String ar[])
+	{
+		Scanner s=new Scanner (System.in);
+		System.out.println("Enter     a:");
+		int    a=s.nextInt();
+		System.out.println("Enter     b:");
+		int    b=s.nextInt();
+		int c=a-b;	
+		System.out.println("total="+c);
+	}
+}
+		

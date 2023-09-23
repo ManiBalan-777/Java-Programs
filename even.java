05c6f50eb3ef4977ad2a16c0ac10d195d4b47de7
ManiBalan-777 <jayabalan2475@gmail.com>
2023-09-23 07:02:12 -0700

    even

---
+++ even.java
@@ -0,0 +1,14 @@
+class even
+{
+	public static void main(String ar[])
+	{
+		int n=20;
+		System.out.println("even number:");
+
+		for(int i=0;i<=n;i++)
+		{
+			if(i%2!=0)
+				System.out.println(i);
+		}
+	}
+}

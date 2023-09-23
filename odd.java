aae8bcf38ac7335e12c47663b0d405c22d841219
ManiBalan-777 <jayabalan2475@gmail.com>
2023-09-23 07:05:14 -0700

    odd

--- odd.java
+++ odd.java
@@ -1,13 +1,13 @@
-class even
+class odd
 {
 	public static void main(String ar[])
 	{
 		int n=20;
-		System.out.println("even number:");
+		System.out.println("odd number:");
 
 		for(int i=0;i<=n;i++)
 		{
-			if(i%2s=0)
+			if(i%2!=0)
 				System.out.println(i);
 		}
 	}

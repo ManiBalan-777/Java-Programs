242f9b91baff7d3de06aea9b0dfb5d0a7d519c04
ManiBalan-777 <jayabalan2475@gmail.com>
2023-09-25 09:16:02 -0700

    explicit conversion

---
+++ explicit.java
@@ -0,0 +1,13 @@
+class strtoint
+{
+	public static void main(Strin ar[])
+	{
+		double d=100.04;
+		long l=(long)d;
+		int i=(int)l;
+		System.out.println("Double"+d);
+		System.out.println("Long value"+l);
+		System.out.println("Int value"+i);
+		
+	}
+}

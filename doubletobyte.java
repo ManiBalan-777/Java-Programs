035319edc8816ddf228e6b69ac430eaa1329b809
ManiBalan-777 <jayabalan2475@gmail.com>
2023-09-25 09:33:30 -0700

    conversion

---
+++ doubletobyte.java
@@ -0,0 +1,17 @@
+class strtoint
+{
+	public static void main(Strin ar[])
+	{
+		byte b;
+		double d=100.04;
+		
+		int i=257;
+		
+		System.out.println("int to byte");
+		b=(byte)i;
+		System.out.println("i="+i+"b="+b);
+		b=(byte)d;
+		System.out.println("d="+d+"b="+b);
+		
+	}
+}
